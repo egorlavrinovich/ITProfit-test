c4cca7517b8914ceac879896da18c074d412179e
Egor <egorlavrinovich@gmail.com>
2022-07-29 10:13:14 +0300

    Добавлены стили для инпутов

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,14 @@ function App() {
         <div className='form-wrapper'>
             <div className='form-title'>Свяжитесь с нами</div>
             <form className='form-contaner'>
-                <Input className='input' type='text' placeholder='Имя и Фамилия'/>
-                <Input className='input' type='email' placeholder='Адрес почты'/>
-                <Input className='input' type='tel' placeholder='Номер телефона'/>
-                <Input className="input" type='date'/>
-                <textarea className='input' value={state} onChange={(e)=>setstate(e.target.value)} placeholder='Введите сообещние...'/>
+                <Input Name='Имя и Фамилия' className='text-field__input' type='text' placeholder='asd'/>
+                <Input Name='Адрес почты' className='text-field__input' type='email' placeholder='asd' />
+                <Input Name='Номер телефона' className='text-field__input' type='tel' placeholder='asd'/>
+                <Input Name='Выберете дату' className="text-field__input" type='date'/>
+                <div className="text-field text-field_floating">
+                <textarea  name='text-field__input' value={state} onChange={(e)=>setstate(e.target.value)} placeholder='asd'/>
+                <label className="text-field__label" htmlFor='textarea'>Введите сообщение</label>
+                </div>
                 <button className='form-send'>Отправить</button>
             </form>
         </div>
--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -8,11 +8,15 @@ interface IInput{
     name?:string;
     onBlur?:React.EventHandler<any>;
     className?:string;
+    Name?:string;
 }
 
-const Input:FC<IInput> = ({onBlur,type,name,placeholder,value,onChange,className}) => {
+const Input:FC<IInput> = ({onBlur,type,name,placeholder,value,onChange,className,Name}) => {
     return (
+        <div className="text-field text-field_floating">
         <input className={className} onBlur={onBlur} value={value} type={type} placeholder={placeholder} onChange={onChange} name={name}/>
+            <label className="text-field__label" htmlFor={name}>{Name}</label>
+        </div>
     );
 };
 
